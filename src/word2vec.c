eb14b81888b8c13499e262948ea3e5b3dbc67318
qa276390 <qa276390@gmail.com>
2018-06-29 12:32:29 +0800

    adding neg code

--- src/word2vec.c
+++ src/word2vec.c
@@ -525,6 +525,7 @@ void* TrainModelThread(void* id) {
                     my_p = 1;
                     for (d = 0; d < vocab[word].codelen; d++) {
                         f = 0;
+				 	  //l2 - The index of our output word within the output layer weights
                         l2 = vocab[word].point[d] * layer1_size;
                         // Propagate hidden -> output
                         for (c = 0; c < layer1_size; c++)
@@ -692,9 +693,18 @@ void* TrainModelThread(void* id) {
 	fwrite(syn0, sizeof(float), sizeof(syn0), fsyn0);
 	fclose(fsyn0);
 
-	FILE *fsyn1 = fopen("syn1", "wb");
-	fwrite(syn1, sizeof(float), sizeof(syn1), fsyn1);
-	fclose(fsyn1);
+	if(hs){
+		FILE *fsyn1 = fopen("syn1", "wb");
+		fwrite(syn1, sizeof(float), sizeof(syn1), fsyn1);
+		fclose(fsyn1);
+	}
+	if(negative>0)
+	{
+		FILE *fsyn1 = fopen("syn1neg", "wb");
+		fwrite(syn1neg, sizeof(float), sizeof(syn1neg), fsyn1);
+		fclose(fsyn1);
+	
+	}
 
 
 	//////////////////////////////////////
@@ -708,10 +718,19 @@ void* TrainModelThread(void* id) {
 	FILE *fsyn0 = fopen("syn0", "rb");
 	fread(syn0, sizeof(float), sizeof(syn0), fsyn0);
 	fclose(fsyn0);
-
-	FILE *fsyn1 = fopen("syn1", "rb");
-	fread(syn1, sizeof(float), sizeof(syn1), fsyn1);
-	fclose(fsyn1);
+	
+	if(hs){
+		FILE *fsyn1 = fopen("syn1", "rb");
+		fread(syn1, sizeof(float), sizeof(syn1), fsyn1);
+		fclose(fsyn1);
+	}
+	if(negative>0)
+	{
+		FILE *fsyn1 = fopen("syn1neg", "rb");
+		fread(syn1neg, sizeof(float), sizeof(syn1neg), fsyn1);
+		fclose(fsyn1);
+		
+	}
 	local_iter = 0;
 
 	printf("syn0 size:%d\n",sizeof(syn0));
@@ -862,6 +881,49 @@ void* TrainModelThread(void* id) {
                         // for (c = 0; c < layer1_size; c++)
                         //  syn1[c + l2] += g * neu1[c];
                     }
+                }
+                // NEGATIVE SAMPLING
+                if (negative > 0){
+					my_p = 0;
+					/*
+                    for (d = 0; d < negative + 1; d++) {
+                        if (d == 0) {
+                            target = word;
+                            label = 1;
+                        } else {
+                            next_random =
+                                next_random * (unsigned long long)25214903917 +
+                                11;
+                            target = table[(next_random >> 16) % table_size];
+                            if (target == 0)
+                                target = next_random % (vocab_size - 1) + 1;
+                            if (target == word)
+                                continue;
+                            label = 0;
+                        }*/
+						////////////////
+						target = word;
+						label = 1;
+						////////////////
+
+                        l2 = target * layer1_size;
+                        f = 0;
+                        for (c = 0; c < layer1_size; c++)
+                            f += neu1[c] * syn1neg[c + l2];
+                        if (f > MAX_EXP)
+                            g = (label - 1) * alpha;
+                        else if (f < -MAX_EXP)
+                            g = (label - 0) * alpha;
+                        else
+                            g = (label - expTable[(int)((f + MAX_EXP) *
+                                                        (EXP_TABLE_SIZE /
+                                                         MAX_EXP / 2))]) * alpha;
+
+						my_p =  expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))];
+                       // for (c = 0; c < layer1_size; c++)
+                       //     neu1e[c] += g * syn1neg[c + l2];
+                       // for (c = 0; c < layer1_size; c++)
+                       //     syn1neg[c + l2] += g * neu1[c];
                 }
                 // hidden -> in
                 for (a = b; a < window * 2 + 1 - b; a++)
